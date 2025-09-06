7d5a219b8a2782f06fc18266e0468f70db06ec91
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-09-06 23:50:56 +0300

    Import Counter inside App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,8 @@ import Header from './components/Header';
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
+
 
 
 function App() {
@@ -37,8 +39,11 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
-      <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
+      <Footer />
+      
+     
     </>
   )
 }
