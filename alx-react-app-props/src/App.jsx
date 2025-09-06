26361d479228cf79138e5d50d6296703e086df6c
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-09-07 00:44:00 +0300

    refactor: replace prop drilling with Context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
+function App() {
+  const userData = { name: "Anne Iyvon", email: "anne.iyv@gmail.com" };
+  
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
+
+
+
---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+function MainContent(){
+return(
+<main style={{ 
+      padding: '20px', 
+      backgroundColor: '#e0f7fa', 
+      color: '#00796b', 
+      textAlign: 'left',
+      lineHeight: '1.6'
+    }}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+
+);
+}
+
+export default MainContent;
