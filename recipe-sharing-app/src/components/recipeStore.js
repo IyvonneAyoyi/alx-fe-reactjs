7d0cd07631f10a5f385155f09999bae91ad8e0e6
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-09-13 21:00:45 +0300

    Include actions for updating and deleting recipes

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,20 @@ import create from 'zustand'
 
 export const useRecipeStore = create(set => ({
   recipes: [],
+  // Add Recipe
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+
+  //Delete Recipe by ID
+  deleteRecipe: (id) =>set((state) => ({recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
+    //Update Recipe by ID
+updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
   setRecipes: (recipes) => set({ recipes })
 }));
