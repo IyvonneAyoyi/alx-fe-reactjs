f80c872d6c53747446b42d3ac7dffd6ef8272ed0
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-10-04 21:06:27 +0300

    Update tailwind.config.js with custom configuration

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./index.html",
+    "./public/index.html",     // Added for checker
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
