174baaa41b1862302bc3031ed22932a094051feb
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-10-11 14:35:31 +0300

    Implement nested and dynamic routing for Profile

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 const Profile = () => {
   return (
     <div>
       <h2>User Profile</h2>
+
       <nav>
         <Link to="details">Profile Details</Link> |{" "}
         <Link to="settings">Profile Settings</Link>
       </nav>
-      <Outlet /> {/*Nested routes render here */}
+
+      {/* Nested Routes */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
 };
